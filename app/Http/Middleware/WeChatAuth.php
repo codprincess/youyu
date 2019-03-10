addb57453ae39341849c76a899e639ab5ac695f5
Alan <5303221@gmail.com>
2019-03-10 16:24:32 +0800

    test

---
+++ app/Http/Middleware/WeChatAuth.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+
+class WeChatAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Session::has('userInfo')) {
+            $this->userInfo = \session('userInfo');
+        } else {
+            Log::debug('checkAuth is fail');
+            return redirect(url("auth"));
+        }
+        return $next($request);
+    }
+}
