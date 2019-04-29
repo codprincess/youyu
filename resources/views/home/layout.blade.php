38e599024369c1dd9798c1e18d427181864ae772
jing <714195347@qq.com>
2019-04-29 21:53:17 +0800

    px

--- resources/views/home/layout.blade.php
+++ resources/views/home/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport"  content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="api-token" content="Bearer {{ $apiToken??"" }}">
     <title>控制面板</title>
