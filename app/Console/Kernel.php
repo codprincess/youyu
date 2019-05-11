223d5f3c81b46f5b4a96238a0b9ca58033f2a684
Alan <5303221@gmail.com>
2019-05-11 22:31:04 +0800

    超时订单关闭

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,9 +3,11 @@
 namespace App\Console;
 
 use App\Models\Order;
+use App\Models\VenueTime;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -36,7 +38,16 @@ class Kernel extends ConsoleKernel
         // 每分钟执行一次
         $schedule->call(function () {
             // 关闭10分钟未付款订单
-            DB::update("UPDATE `orders` SET `status`='2'  WHERE (`created_at` < DATE_ADD(NOW(), INTERVAL -10 MINUTE) AND `status` = 1);");
+            $orders = DB::select("select `id`,`venue_time_ids` from `orders` WHERE (`created_at` < DATE_ADD(NOW(), INTERVAL -10 MINUTE) AND `status` = 1)");
+            foreach ($orders as $order){
+                Order::where('id', $order->id)->update(['status' => 3]);
+                $ids = explode(',', $order->venue_time_ids);
+                foreach ($ids as $id) {
+                    VenueTime::where('id', $id)->update(['status' => 1]);
+                    echo sprintf("订单超时关闭:%d \n", $id);
+                }
+            }
+            // 释放场次
         })->everyMinute();
 
     }
